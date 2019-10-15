95292f3e6b5ebf238da43bc6217652d1ec18b413
oliveira-andre <desmonplays@gmail.com>
2019-10-15 14:06:13 -0400

    (new feature) generating table adversiments

---
+++ db/migrate/20191015180055_advertisements.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Advertisements < ActiveRecord::Migration[5.2]
+  def change
+    create_table :advertisements do |t|
+      t.string :title, null: false
+      t.string :description
+      t.decimal :value, null: false, precision: 10, scale: 2
+      t.references :establishment, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
