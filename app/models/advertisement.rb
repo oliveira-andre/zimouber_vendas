30f42122a9d406b178b4257250aabd77bb40827f
oliveira-andre <desmonplays@gmail.com>
2019-10-15 16:00:31 -0400

    (new feature) applying friendly id

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Advertisement < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   has_one_attached :image
 
   validates :title, presence: true, length: { minimum: 2 }
--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Establishment < ApplicationRecord
+  extend FriendlyId
+  friendly_id :trading_name, use: :slugged
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
