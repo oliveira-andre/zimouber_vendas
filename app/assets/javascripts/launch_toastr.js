ab4b5740df23430e3e8d1cb202ff1fb48aa19d9d
oliveira-andre <desmonplays@gmail.com>
2019-10-15 18:38:52 -0400

    (new feature) launch toastr on js

---
+++ app/assets/javascripts/launch_toastr.js
@@ -0,0 +1,25 @@
+$(document).on("turbolinks:load", function () {
+  var flashtoastr = $('.flashtoastr-item');
+  if (!flashtoastr) return;
+
+  flashtoastr.each(function (index) {
+    var target = $('.flashtoastr-item')[index];
+
+    switch (target.dataset.type) {
+      case 'notice':
+        toastr.info(target.textContent);
+        break;
+      case 'alert':
+        toastr.warning(target.textContent);
+        break;
+      case 'error':
+        toastr.error(target.textContent);
+        break;
+      case 'success':
+        toastr.success(target.textContent);
+        break;
+      default:
+        toastr.info(target.textContent);
+    }
+  });
+});
