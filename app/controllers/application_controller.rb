cb60fa881779ab918df92c97c192fe5ad46faec5
oliveira-andre <desmonplays@gmail.com>
2019-10-15 15:59:51 -0400

    (new feature) trading as a permitted parameter on sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_establishment!
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: %i[trading_name]
+    )
+  end
 end
