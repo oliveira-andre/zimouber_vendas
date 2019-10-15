083740f9f37d58495988d6a380777546ae3d09a0
oliveira-andre <desmonplays@gmail.com>
2019-10-15 18:50:07 -0400

    (new feature) helper mask_money

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def mask_money(value)
+    integer = value.to_s.split('.').first
+    decimal = value.to_s.split('.').last
+    decimal = "#{decimal}0" if decimal.length == 1
+    "R$ #{integer},#{decimal}"
+  end
 end
