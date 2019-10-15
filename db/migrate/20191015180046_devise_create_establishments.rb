434fd90768c6f41ae03ef183907bb4c8dd3dce54
oliveira-andre <desmonplays@gmail.com>
2019-10-15 14:16:20 -0400

    (new feature) correct trading name

--- db/migrate/20191015180046_devise_create_establishments.rb
+++ db/migrate/20191015180046_devise_create_establishments.rb
@@ -6,7 +6,7 @@ class DeviseCreateEstablishments < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
-      t.string :fantasy_name,       null: false, default: ''
+      t.string :trading_name,       null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
