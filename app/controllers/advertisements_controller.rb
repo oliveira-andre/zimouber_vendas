8dcd09796ac6389235eff251e4242222ec4fb85b
oliveira-andre <desmonplays@gmail.com>
2019-10-15 21:47:58 -0400

    (new feature) loading ad by the friendly id and authorizing

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AdvertisementsController < ApplicationController
+  before_action :load_advertisement, only: %i[edit update show]
+
   def index
     @ads = current_establishment.advertisements
   end
@@ -22,10 +24,29 @@ class AdvertisementsController < ApplicationController
     redirect_to new_advertisement_path
   end
 
+  def edit
+    authorize @ad
+  end
+
+  def update
+    authorize @ad
+    @ad.update!(advertisement_params)
+    flash[:success] = t('form.advertisement.update_success')
+    redirect_to advertisements_path
+  rescue ActiveRecord::RecordInvalid => e
+    e.record.errors.full_messages.each { |msg| flash[:error] = msg }
+    redirect_to edit_advertisement_path(@ad)
+  end
+
   private
 
   def advertisement_params
-    params.require(:advertisement).permit(:heading, :value, :description)
-          .merge(establishment: current_establishment)
+    params.require(:advertisement).permit(
+      :image, :heading, :value, :description
+    ).merge(establishment: current_establishment)
+  end
+
+  def load_advertisement
+    @ad = Advertisement.friendly.find(params[:id])
   end
 end
