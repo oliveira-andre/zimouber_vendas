0a9e74925c6e633615195df36a88587cc73b82e8
oliveira-andre <desmonplays@gmail.com>
2019-10-15 21:33:58 -0400

    (new feature) rescue from, not authorized and not found / rewriting the method pundit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized_error
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_establishment!
 
@@ -11,5 +14,19 @@ class ApplicationController < ActionController::Base
       :sign_up,
       keys: %i[trading_name]
     )
+  end
+
+  def not_found_error
+    redirect_to back_url
+    flash[:error] = t('not_found')
+  end
+
+  def not_authorized_error
+    redirect_to back_url
+    flash[:error] = t('not_authorized')
+  end
+
+  def pundit_user
+    current_establishment
   end
 end
