8ee905e97ddaf6caa5a3e7a31579f90945924046
oliveira-andre <desmonplays@gmail.com>
2019-10-15 21:33:34 -0400

    (new feature) create with params and exception error, adding aprams and authorizing

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -7,5 +7,25 @@ class AdvertisementsController < ApplicationController
 
   def new
     @ad = Advertisement.new(establishment: current_establishment)
+    authorize @ad
+  end
+
+  def create
+    @ad = Advertisement.new(advertisement_params)
+    authorize @ad
+    @ad.save!
+    @ad.image.attach(params[:advertisement][:image])
+    flash[:success] = t('form.advertisement.create_success')
+    redirect_to advertisements_path
+  rescue ActiveRecord::RecordInvalid => e
+    e.record.errors.full_messages.each { |msg| flash[:error] = msg }
+    redirect_to new_advertisement_path
+  end
+
+  private
+
+  def advertisement_params
+    params.require(:advertisement).permit(:heading, :value, :description)
+          .merge(establishment: current_establishment)
   end
 end
